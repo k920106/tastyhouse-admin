7977c48b6e3dc58656b7705c0d834fafb657dc87
kk920106 <kk920106@naver.com>
2025-09-24 07:44:00 +0900

    NoticeColumns 제거 후 NoticeList 컴포넌트에 병합

--- src/app/(dashboard)/notices/(list)/_components/NoticeList.tsx
+++ src/app/(dashboard)/notices/(list)/_components/NoticeList.tsx
@@ -2,8 +2,59 @@
 
 import { CommonDataTable } from '@/src/components/shared/CommonDataTable'
 import { useNoticeSearchWithQuery } from '@/src/hooks/notice/useNoticeSearchWithQuery'
+import { formatToYYYYMMDD } from '@/src/lib/date-utils'
+import { NoticeListItem, getNoticeUseStatusLabel } from '@/src/types/notice'
+import { ColumnDef } from '@tanstack/react-table'
 import { useCallback } from 'react'
-import { createColumns } from './NoticeColumns'
+
+const createColumns = (currentPage: number, pageSize: number): ColumnDef<NoticeListItem>[] => [
+  {
+    id: 'rowNumber',
+    header: 'No.',
+    meta: {
+      className: 'border-x text-center',
+    },
+    cell: ({ row }) => (
+      <div aria-label={`${currentPage * pageSize + row.index + 1}번째 행`}>
+        {currentPage * pageSize + row.index + 1}
+      </div>
+    ),
+  },
+  {
+    accessorKey: 'companyName',
+    header: '매체사',
+    meta: {
+      className: 'border-x',
+    },
+  },
+  {
+    accessorKey: 'title',
+    header: '제목',
+    meta: {
+      className: 'border-x',
+    },
+  },
+  {
+    accessorKey: 'createdAt',
+    header: '등록일자',
+    cell: ({ row }) => {
+      return <div>{formatToYYYYMMDD(row.original.createdAt)}</div>
+    },
+    meta: {
+      className: 'border-x',
+    },
+  },
+  {
+    accessorKey: 'isUse',
+    header: '사용 여부',
+    cell: ({ row }) => {
+      return <div>{getNoticeUseStatusLabel(row.original.isUse)}</div>
+    },
+    meta: {
+      className: 'border-x',
+    },
+  },
+]
 
 export default function NoticeList() {
   const { currentPage, pageSize, urlSearchForm, updateUrl, data, isLoading } =
