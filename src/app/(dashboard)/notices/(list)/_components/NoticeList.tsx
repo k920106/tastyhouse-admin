95b825d9705c48c1237c7e1f98994e307627ff85
kk920106 <kk920106@naver.com>
2025-10-02 09:20:24 +0900

    리팩토링: NoticeList 구성 요소에서 useMemo를 제거하고 상수로 NOTICE_COLUMNS 정의하여 코드 간결성 개선

--- src/app/(dashboard)/notices/(list)/_components/NoticeList.tsx
+++ src/app/(dashboard)/notices/(list)/_components/NoticeList.tsx
@@ -1,61 +1,59 @@
 'use client'
 
+import { useCallback } from 'react'
+
 import { CommonDataTable } from '@/src/components/shared/CommonDataTable'
 import { useNoticeSearchContext } from '@/src/contexts/NoticeSearchContext'
 import { formatToYYYYMMDD } from '@/src/lib/date-utils'
 import { NoticeListItem, getNoticeActiveStatusLabel } from '@/src/types/notice'
 import { ColumnDef } from '@tanstack/react-table'
-import { useCallback, useMemo } from 'react'
+
+const NOTICE_COLUMNS: ColumnDef<NoticeListItem>[] = [
+  {
+    id: 'rowNumber',
+    header: 'No.',
+    meta: {
+      className: 'border-x text-center',
+    },
+    cell: ({ row, table }) => {
+      const state = table.getState().pagination
+      return <div>{state.pageIndex * state.pageSize + row.index + 1}</div>
+    },
+  },
+  {
+    accessorKey: 'companyName',
+    header: '매체사',
+    meta: {
+      className: 'border-x',
+    },
+  },
+  {
+    accessorKey: 'title',
+    header: '제목',
+    meta: {
+      className: 'border-x',
+    },
+  },
+  {
+    accessorKey: 'createdAt',
+    header: '등록일자',
+    cell: ({ row }) => <div>{formatToYYYYMMDD(row.original.createdAt)}</div>,
+    meta: {
+      className: 'border-x',
+    },
+  },
+  {
+    accessorKey: 'active',
+    header: '활성 여부',
+    cell: ({ row }) => <div>{getNoticeActiveStatusLabel(row.original.active)}</div>,
+    meta: {
+      className: 'border-x',
+    },
+  },
+]
 
 export default function NoticeList() {
   const { currentPage, pageSize, updateUrl, data, isLoading } = useNoticeSearchContext()
-
-  const columns: ColumnDef<NoticeListItem>[] = useMemo(
-    () => [
-      {
-        id: 'rowNumber',
-        header: 'No.',
-        meta: {
-          className: 'border-x text-center',
-        },
-        cell: ({ row, table }) => {
-          const state = table.getState().pagination
-          return <div>{state.pageIndex * state.pageSize + row.index + 1}</div>
-        },
-      },
-      {
-        accessorKey: 'companyName',
-        header: '매체사',
-        meta: {
-          className: 'border-x',
-        },
-      },
-      {
-        accessorKey: 'title',
-        header: '제목',
-        meta: {
-          className: 'border-x',
-        },
-      },
-      {
-        accessorKey: 'createdAt',
-        header: '등록일자',
-        cell: ({ row }) => <div>{formatToYYYYMMDD(row.original.createdAt)}</div>,
-        meta: {
-          className: 'border-x',
-        },
-      },
-      {
-        accessorKey: 'active',
-        header: '활성 여부',
-        cell: ({ row }) => <div>{getNoticeActiveStatusLabel(row.original.active)}</div>,
-        meta: {
-          className: 'border-x',
-        },
-      },
-    ],
-    [],
-  )
 
   const handlePageChange = useCallback(
     (newPage: number, newPageSize?: number) => {
@@ -66,7 +64,7 @@ export default function NoticeList() {
 
   return (
     <CommonDataTable
-      columns={columns}
+      columns={NOTICE_COLUMNS}
       data={data?.notices || []}
       totalCount={data?.totalElements || 0}
       currentPage={currentPage}
